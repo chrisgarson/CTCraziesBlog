b2637cb804cb7a165b8873a71a69f3d65a51f5da
chrisgarson <chrisgarson@zohomail.com>
2026-07-19 14:37:52 -0500

    Update client/src/pages/Page16.tsx: Democrat Party tag removal and tag fixes

--- client/src/pages/Page16.tsx
+++ client/src/pages/Page16.tsx
@@ -11,14 +11,14 @@ export default function Home() {
         tinyUrl="https://tinyurl.com/mr2ddr8x"
         xPostUrl="https://x.com/C3Heditor/status/2060090787921121306"
         imageSrc="https://cdn.jsdelivr.net/gh/chrisgarson/CTCraziesBlog@main/article-images/2026-05-28_150726_51b8b897.jpg"
-        tags={["Democrat Party", "Terrorism", "Maine", "2026 Elections"]}
+        tags={["Terrorism", "Maine", "2026 Elections"]}
       />
       <ArticleBlock
         headline="California Democrats Fight to Shield Immigrant Truckers From English Proficiency Test Required for Commercial Drivers"
         tinyUrl="https://tinyurl.com/yuu7jejx"
         xPostUrl="https://x.com/C3Heditor/status/2060089932203344336"
         imageSrc="https://cdn.jsdelivr.net/gh/chrisgarson/CTCraziesBlog@main/article-images/2026-05-28_150526_7aba8861.jpg"
-        tags={["California", "Immigration", "Democrat Party", "Culture War"]}
+        tags={["California", "Immigration", "Culture War"]}
       />
       <ArticleBlock
         headline="Federal Guilty Plea Now Confirms Los Angeles Voter Fraud That the Democrats Have Long Claimed Never Happens"
@@ -32,21 +32,21 @@ export default function Home() {
         tinyUrl="https://tinyurl.com/3fr5c39r"
         xPostUrl="https://x.com/C3Heditor/status/2060088076827492617"
         imageSrc="https://cdn.jsdelivr.net/gh/chrisgarson/CTCraziesBlog@main/article-images/2026-05-28_150102_c965ea38.jpg"
-        tags={["California", "Election Fraud", "Democrat Party"]}
+        tags={["California", "Election Fraud"]}
       />
       <ArticleBlock
         headline="Election Top-2 Primary Rules That Crushed Republicans Are Now Suddenly &#39;Unfair&#39; When Democrats Face Them In California"
         tinyUrl="https://tinyurl.com/2t67erep"
         xPostUrl="https://x.com/C3Heditor/status/2060087141688082621"
         imageSrc="https://cdn.jsdelivr.net/gh/chrisgarson/CTCraziesBlog@main/article-images/2026-05-28_145309_308a8de1.jpg"
-        tags={["California", "Democrat Party", "Election Fraud", "2024 Election"]}
+        tags={["California", "Election Fraud", "2024 Election"]}
       />
       <ArticleBlock
         headline="Wildly False Claim by Texas U.S. Senate Candidate James Talarico: Climate Change Is Killing Thousands of Texans &amp; Americans"
         tinyUrl="https://tinyurl.com/2fs45p28"
         xPostUrl="https://x.com/C3Heditor/status/2060086290753228889"
         imageSrc="https://cdn.jsdelivr.net/gh/chrisgarson/CTCraziesBlog@main/article-images/2026-05-28_144826_53c57631.jpg"
-        tags={["Texas", "Democrat Party", "Climate Change", "2026 Elections"]}
+        tags={["Texas", "Climate Change", "2026 Elections"]}
       />
       <ArticleBlock
         headline="Oregon&#39;s Woke Democrat Economics: Rock-Bottom Sale of Portland&#39;s Flagship Office Tower Shows a City In The Tank"
@@ -60,91 +60,91 @@ export default function Home() {
         tinyUrl="https://tinyurl.com/mr3phrwb"
         xPostUrl="https://x.com/C3Heditor/status/2060084420474499137"
         imageSrc="https://cdn.jsdelivr.net/gh/chrisgarson/CTCraziesBlog@main/article-images/2026-05-28_144156_b637c30a.jpg"
-        tags={["Violent Crime", "California", "Criminal Justice", "Democrat Party"]}
+        tags={["Violent Crime", "California", "Criminal Justice"]}
       />
       <ArticleBlock
         headline="Connecticut Democrats Push A Homeschool Crackdown That Attacks School Choice &amp; Parental Rights"
         tinyUrl="https://tinyurl.com/2zy269f4"
         xPostUrl="https://x.com/C3Heditor/status/2060083485335134261"
         imageSrc="https://cdn.jsdelivr.net/gh/chrisgarson/CTCraziesBlog@main/article-images/2026-05-28_143925_b13fee5c.jpg"
-        tags={["Connecticut", "Education Policy", "Culture War", "Democrat Party"]}
+        tags={["Connecticut", "Education Policy", "Culture War"]}
       />
       <ArticleBlock
         headline="Democrat Rep. Ro Khanna Wants To Pack SCOTUS With Four More Justices Just Like Progressive-Left Ketanji Brown"
         tinyUrl="https://tinyurl.com/c54d7f42"
         xPostUrl="https://x.com/C3Heditor/status/2060082550195769385"
         imageSrc="https://cdn.jsdelivr.net/gh/chrisgarson/CTCraziesBlog@main/article-images/2026-05-28_142928_51e1efe5.jpg"
-        tags={["Judiciary-Judicial", "Democrat Party", "Left-Wing", "Corruption-Fraud"]}
+        tags={["Judiciary-Judicial", "Left-Wing", "Corruption-Fraud"]}
       />
       <ArticleBlock
         headline="Colorado Ranching Area&#39;s 8th Congressional District&#39;s Democrat Candidate Is a Vegan With Deep Anti-Meat Convictions"
         tinyUrl="https://tinyurl.com/ytef72zt"
         xPostUrl="https://x.com/C3Heditor/status/2060080027195015420"
         imageSrc="https://cdn.jsdelivr.net/gh/chrisgarson/CTCraziesBlog@main/article-images/2026-05-28_142609_aaf49e69.jpg"
-        tags={["Colorado", "Democrat Party", "Climate Change", "Left-Wing"]}
+        tags={["Colorado", "Climate Change", "Left-Wing"]}
       />
       <ArticleBlock
         headline="Democrat Election Denial Continues: Insists Trump Lost 2024 Swing States Due To Elon&#39;s Malware"
         tinyUrl="https://tinyurl.com/s92zsex6"
         xPostUrl="https://x.com/C3Heditor/status/2060079347017347401"
         imageSrc="https://cdn.jsdelivr.net/gh/chrisgarson/CTCraziesBlog@main/article-images/2026-05-28_142223_67790c17.jpg"
-        tags={["Democrat Party", "2024 Election", "Election Fraud", "Left-Wing"]}
+        tags={["2024 Election", "Election Fraud", "Left-Wing"]}
       />
       <ArticleBlock
         headline="Mounting Scandals Prompt Democrats to Abandon Maine Senate Hopeful, Nazi-Associated Graham Platner"
         tinyUrl="https://tinyurl.com/yc5hypmm"
         xPostUrl="https://x.com/C3Heditor/status/2060078360072450105"
         imageSrc="https://cdn.jsdelivr.net/gh/chrisgarson/CTCraziesBlog@main/article-images/2026-05-28_141928_4a320661.jpg"
-        tags={["Maine", "Democrat Party", "2026 Elections", "Corruption-Fraud"]}
+        tags={["Maine", "2026 Elections", "Corruption-Fraud"]}
       />
       <ArticleBlock
         headline="House Probe Reveals Leftist Law Firm Defending China-Based Singham Network of Democrat Protestors Has Deep Marxist Ties"
         tinyUrl="https://tinyurl.com/5yyekudd"
         xPostUrl="https://x.com/C3Heditor/status/2060076497767927959"
         imageSrc="https://cdn.jsdelivr.net/gh/chrisgarson/CTCraziesBlog@main/article-images/2026-05-28_141450_7398b328.jpg"
-        tags={["Left-Wing", "China Policy", "Corruption-Fraud", "Democrat Party"]}
+        tags={["Left-Wing", "China Policy", "Corruption-Fraud"]}
       />
       <ArticleBlock
         headline="A Closer Look Back at the Long &amp; Damning Record of Democrats&#39; Blatant Lies"
         tinyUrl="https://tinyurl.com/2s445ye5"
         xPostUrl="https://x.com/C3Heditor/status/2060075165992484905"
         imageSrc="https://cdn.jsdelivr.net/gh/chrisgarson/CTCraziesBlog@main/article-images/2026-05-28_140704_38a8c6b9.jpg"
-        tags={["Democrat Party", "Left-Wing", "Corruption-Fraud", "Racial Issues"]}
+        tags={["Left-Wing", "Corruption-Fraud", "Racial Issues"]}
       />
       <ArticleBlock
         headline="Gov. Hochul &amp; State Democrats Hand NYC Taxpayers a Large $151M Bill by Rolling Back Public-Sector Pension Reforms"
         tinyUrl="https://tinyurl.com/5ft7v7uy"
         xPostUrl="https://x.com/C3Heditor/status/2060074244747202795"
         imageSrc="https://cdn.jsdelivr.net/gh/chrisgarson/CTCraziesBlog@main/article-images/2026-05-28_140320_a319b0dd.jpg"
-        tags={["New York", "Democrat Party", "Government"]}
+        tags={["New York", "Government"]}
       />
       <ArticleBlock
         headline="Xavier Becerra, Democrat Candidate for California Governor, Pushes Free Healthcare for Illegal Aliens While Boasting About Jobs They Take"
         tinyUrl="https://tinyurl.com/4efw2wj9"
         xPostUrl="https://x.com/C3Heditor/status/2060073406419144734"
         imageSrc="https://cdn.jsdelivr.net/gh/chrisgarson/CTCraziesBlog@main/article-images/2026-05-28_135944_dca1db4a.jpg"
-        tags={["California", "Immigration", "Healthcare Policy", "Democrat Party"]}
+        tags={["California", "Immigration", "Healthcare Policy"]}
       />
       <ArticleBlock
         headline="AOC&#39;s Choice of Democrat Candidate Worked With Al-Qaeda &amp; Defended World Trade Center Blind Sheikh Bomber"
         tinyUrl="https://tinyurl.com/bdz5yrwe"
         xPostUrl="https://x.com/C3Heditor/status/2060072480157388930"
         imageSrc="https://cdn.jsdelivr.net/gh/chrisgarson/CTCraziesBlog@main/article-images/2026-05-28_135600_8fbcd6f3.jpg"
-        tags={["Democrat Party", "Terrorism", "Corruption-Fraud", "Foreign Policy"]}
+        tags={["Terrorism", "Corruption-Fraud", "Foreign Policy"]}
       />
       <ArticleBlock
         headline="Hakeem Jeffries Reveals Democrats&#39; Goal On Trump Voters: &#39;Our Mission Is To Break Them&#39;"
         tinyUrl="https://tinyurl.com/34yrshcp"
         xPostUrl="https://x.com/C3Heditor/status/2060071849245942156"
         imageSrc="https://cdn.jsdelivr.net/gh/chrisgarson/CTCraziesBlog@main/article-images/2026-05-28_134717_3283a43b.jpg"
-        tags={["Democrat Party", "Republican Party", "2024 Election", "Racial Issues"]}
+        tags={["Republican Party", "2024 Election", "Racial Issues"]}
       />
       <ArticleBlock
         headline="Hasan Kwame Jeffries, Brother of Top Democrat Hakeem Jeffries, Advocates Violence Against Whites"
         tinyUrl="https://tinyurl.com/3vsf8vse"
         xPostUrl="https://x.com/C3Heditor/status/2060010202892480843"
         imageSrc="https://cdn.jsdelivr.net/gh/chrisgarson/CTCraziesBlog@main/article-images/2026-05-28_094913_242b156e.jpg"
-        tags={["Racial Issues", "Democrat Party", "Left-Wing", "Culture War"]}
+        tags={["Racial Issues", "Left-Wing", "Culture War"]}
       />
       </div>
       <Pagination currentPage={16} totalPages={61} />
