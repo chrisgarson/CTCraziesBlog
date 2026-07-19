cd19a2ea374c3b7f3ab30a3fcea3a901ebe13b2b
chrisgarson <chrisgarson@zohomail.com>
2026-07-19 14:37:54 -0500

    Update client/src/pages/Page19.tsx: Democrat Party tag removal and tag fixes

--- client/src/pages/Page19.tsx
+++ client/src/pages/Page19.tsx
@@ -11,7 +11,7 @@ export default function Home() {
         tinyUrl="https://tinyurl.com/3jdj6d3x"
         xPostUrl="https://x.com/C3Heditor/status/2056476424626679964"
         imageSrc="https://cdn.jsdelivr.net/gh/chrisgarson/CTCraziesBlog@main/article-images/2026-05-18_154633_8fceee6f.jpg"
-        tags={["Virginia", "Judiciary-Judicial", "Democrat Party", "2026 Elections"]}
+        tags={["Virginia", "Judiciary-Judicial", "2026 Elections"]}
       />
       <ArticleBlock
         headline="Democrat Partisan Judicial System Hates Law &amp; Order: Out-of-Control Judges Lead To Out-of-Control Crime"
@@ -25,14 +25,14 @@ export default function Home() {
         tinyUrl="https://tinyurl.com/4udkwhmx"
         xPostUrl="https://x.com/C3Heditor/status/2056475287995461979"
         imageSrc="https://cdn.jsdelivr.net/gh/chrisgarson/CTCraziesBlog@main/article-images/2026-05-18_154214_1618b1a2.jpg"
-        tags={["Democrat Party", "National Security", "Islam-Muslim"]}
+        tags={["National Security", "Islam-Muslim"]}
       />
       <ArticleBlock
         headline="It&#39;s Another Democrat Liar: Michigan&#39;s U.S. Senate Hopeful Abdul El-Sayed Claims He&#39;s A Physician But Has No Medical License"
         tinyUrl="https://tinyurl.com/msarz7nt"
         xPostUrl="https://x.com/C3Heditor/status/2056474697420657017"
         imageSrc="https://cdn.jsdelivr.net/gh/chrisgarson/CTCraziesBlog@main/article-images/2026-05-18_153948_afc67beb.jpg"
-        tags={["Michigan", "Democrat Party", "2026 Elections", "Healthcare Policy"]}
+        tags={["Michigan", "2026 Elections", "Healthcare Policy"]}
       />
       <ArticleBlock
         headline="SCOTUS Kills Virginia&#39;s Wildly Illegitimate Gerrymander Redistricting Plan Causing Gov. Spanberger To Rage On &#39;X&#39;"
@@ -46,7 +46,7 @@ export default function Home() {
         tinyUrl="https://tinyurl.com/ms8v6ca4"
         xPostUrl="https://x.com/C3Heditor/status/2056471328572551392"
         imageSrc="https://cdn.jsdelivr.net/gh/chrisgarson/CTCraziesBlog@main/article-images/2026-05-18_152621_0cfb17fa.jpg"
-        tags={["2026 Elections", "Democrat Party"]}
+        tags={["2026 Elections", "Corruption-Fraud"]}
       />
       <ArticleBlock
         headline="Kamala Harris Lays Out The Simple Democrat Anti-Democracy Agenda: Pack The Courts, Add D.C. As A State &amp; Kill The Electoral College"
@@ -60,7 +60,7 @@ export default function Home() {
         tinyUrl="https://tinyurl.com/29f3eyjx"
         xPostUrl="https://x.com/C3Heditor/status/2056469820724384057"
         imageSrc="https://cdn.jsdelivr.net/gh/chrisgarson/CTCraziesBlog@main/article-images/2026-05-18_152024_31e750fa.jpg"
-        tags={["Democrat Party", "2026 Elections", "Politics"]}
+        tags={["2026 Elections", "Politics"]}
       />
       <ArticleBlock
         headline="Oregon Voters Not Happy Over Democrats&#39; New Gas Tax Piled On Top of Rising Gas Prices"
@@ -74,7 +74,7 @@ export default function Home() {
         tinyUrl="https://tinyurl.com/42bzzf7b"
         xPostUrl="https://x.com/C3Heditor/status/2056467080115900427"
         imageSrc="https://cdn.jsdelivr.net/gh/chrisgarson/CTCraziesBlog@main/article-images/2026-05-18_151015_b92f0009.jpg"
-        tags={["California", "Election Fraud", "2026 Elections", "Democrat Party"]}
+        tags={["California", "Election Fraud", "2026 Elections"]}
       />
       <ArticleBlock
         headline="U.S. House Beg Sec of State Rubio To Treat Narco Criminals &amp; Terrorists With More Kindness And Respect"
@@ -88,14 +88,14 @@ export default function Home() {
         tinyUrl="https://tinyurl.com/42yc5y5m"
         xPostUrl="https://x.com/C3Heditor/status/2056461222120439820"
         imageSrc="https://cdn.jsdelivr.net/gh/chrisgarson/CTCraziesBlog@main/article-images/2026-05-18_144608_eb3d3c13.jpg"
-        tags={["Climate Change", "Democrat Party", "Politics"]}
+        tags={["Climate Change", "Politics"]}
       />
       <ArticleBlock
         headline="Black Congressman In A White-Majority District Publicly Torches Democrat Party Over Their Absurd &#39;Jim Crow&#39; Rhetoric"
         tinyUrl="https://tinyurl.com/2s3jyy7e"
         xPostUrl="https://x.com/C3Heditor/status/2056460326959567255"
         imageSrc="https://cdn.jsdelivr.net/gh/chrisgarson/CTCraziesBlog@main/article-images/2026-05-18_144248_84bb781e.jpg"
-        tags={["Racial Issues", "Democrat Party", "Conservative Politics"]}
+        tags={["Racial Issues", "Conservative Politics"]}
       />
       <ArticleBlock
         headline="TMZ Founder: Admits Los Angeles Has Completely Fallen Apart Under The Democrats He Supported"
@@ -109,7 +109,7 @@ export default function Home() {
         tinyUrl="https://tinyurl.com/hv4wwzvu"
         xPostUrl="https://x.com/C3Heditor/status/2056459126738391434"
         imageSrc="https://cdn.jsdelivr.net/gh/chrisgarson/CTCraziesBlog@main/article-images/2026-05-18_143750_ea3d5832.jpg"
-        tags={["Media Bias", "Democrat Party", "Virginia"]}
+        tags={["Media Bias", "Virginia"]}
       />
       <ArticleBlock
         headline="Aiding Communist Dictators: Michigan&#39;s Democrat Rep. Jayapal Admits Helping Cuba Dodge The U.S. Oil Blockade"
@@ -123,7 +123,7 @@ export default function Home() {
         tinyUrl="https://tinyurl.com/4ny4tx3d"
         xPostUrl="https://x.com/C3Heditor/status/2056457437952274530"
         imageSrc="https://cdn.jsdelivr.net/gh/chrisgarson/CTCraziesBlog@main/article-images/2026-05-18_143111_f658c21f.jpg"
-        tags={["Democrat Party", "Media Bias", "2024 Election"]}
+        tags={["Media Bias", "2024 Election"]}
       />
       <ArticleBlock
         headline="Brooklyn Is Now Ground Zero For The AntiSemite Intifada Under New York City&#39;s Democrat Marxist Mayor Mamdani"
