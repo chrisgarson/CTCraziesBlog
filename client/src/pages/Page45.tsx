952ae4de34a7caed8b479d33980ec4acff1cc831
chrisgarson <chrisgarson@zohomail.com>
2026-07-19 14:38:16 -0500

    Update client/src/pages/Page45.tsx: Democrat Party tag removal and tag fixes

--- client/src/pages/Page45.tsx
+++ client/src/pages/Page45.tsx
@@ -60,14 +60,14 @@ export default function Page16() {
         tinyUrl="https://tinyurl.com/zmfmx6zr"
         xPostUrl="https://x.com/C3Heditor/status/2036879876037189632"
         imageSrc="https://d2xsxph8kpxj0f.cloudfront.net/310419663027968276/diaZZv9B9aBWPyk7ymHmxE/2026-03-25_135751_561db0ee.jpg"
-        tags={["Gavin Newsom"]}
+        tags={["Gavin Newsom", "Violent Crime"]}
       />
       <ArticleBlock
         headline="U.S. House Drops a Report Documenting How Democrat-Run American Colleges Have Become Antisemitic Hotbeds"
         tinyUrl="https://tinyurl.com/4sw54t4m"
         xPostUrl="https://x.com/C3Heditor/status/2036878957501440335"
         imageSrc="https://d2xsxph8kpxj0f.cloudfront.net/310419663027968276/diaZZv9B9aBWPyk7ymHmxE/2026-03-25_135322_88d85182.jpg"
-        tags={["Censorship"]}
+        tags={["Censorship", "Education Policy", "Israel"]}
       />
       <ArticleBlock
         headline="Democrat Partisan-Judge Frees a 4-Time-Deported MS-13 Gang Member With a Documented Rape History"
@@ -81,7 +81,7 @@ export default function Page16() {
         tinyUrl="https://tinyurl.com/yu9u45cn"
         xPostUrl="https://x.com/C3Heditor/status/2036846215875137991"
         imageSrc="https://d2xsxph8kpxj0f.cloudfront.net/310419663027968276/diaZZv9B9aBWPyk7ymHmxE/2026-03-25_114322_1a8f166a.jpg"
-        tags={["Arizona"]}
+        tags={["Arizona", "Politics"]}
       />
       <ArticleBlock
         headline="Minnesota Democrats Pass Harsh Gun Laws After Silencing the State&#39;s Largest Gun Rights Group"
@@ -116,7 +116,7 @@ export default function Page16() {
         tinyUrl="https://tinyurl.com/yfbjh7fh"
         xPostUrl="https://x.com/C3Heditor/status/2036839826041782517"
         imageSrc="https://d2xsxph8kpxj0f.cloudfront.net/310419663027968276/diaZZv9B9aBWPyk7ymHmxE/2026-03-25_111821_d1a12857.jpg"
-        tags={["Virginia", "Election Fraud", "Democrat Party", "Politics"]}
+        tags={["Virginia", "Election Fraud", "Politics"]}
       />
       <ArticleBlock
         headline="Even In Democrat One-Party California Children Have Free Speech Rights To Say &#39;All Lives Matter&#39;"
@@ -130,7 +130,7 @@ export default function Page16() {
         tinyUrl="https://tinyurl.com/y7at77mn"
         xPostUrl="https://x.com/C3Heditor/status/2036837472642986351"
         imageSrc="https://d2xsxph8kpxj0f.cloudfront.net/310419663027968276/diaZZv9B9aBWPyk7ymHmxE/2026-03-25_110846_e1d37834.jpg"
-        tags={["Maxine Waters"]}
+        tags={["Maxine Waters", "Patriotism"]}
       />
       <ArticleBlock
         headline="Another Democrat Illegal Alien: Tren de Aragua&#39;s Leader &#39;El Turko&#39; Nabbed in LA on Murder &amp; Kidnapping Charges"
@@ -144,7 +144,7 @@ export default function Page16() {
         tinyUrl="https://tinyurl.com/r5znwpm2"
         xPostUrl="https://x.com/C3Heditor/status/2036803605467185303"
         imageSrc="https://d2xsxph8kpxj0f.cloudfront.net/310419663027968276/diaZZv9B9aBWPyk7ymHmxE/2026-03-25_085401_1bfba604.jpg"
-        tags={["Los Angeles", "Corruption-Fraud", "Government", "Democrat Party"]}
+        tags={["Los Angeles", "Corruption-Fraud", "Government"]}
       />
       <Pagination currentPage={45} totalPages={61} />
     </div>
