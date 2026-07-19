1ff48ef4bf6e4ebc3c4c06471447d51e4ff19392
chrisgarson <chrisgarson@zohomail.com>
2026-07-19 14:38:03 -0500

    Update client/src/pages/Page3.tsx: Democrat Party tag removal and tag fixes

--- client/src/pages/Page3.tsx
+++ client/src/pages/Page3.tsx
@@ -32,28 +32,28 @@ export default function Page2() {
         tinyUrl="https://tinyurl.com/4pdbjhay"
         xPostUrl="https://x.com/C3Heditor/status/2075589279682634022"
         imageSrc="https://cdn.jsdelivr.net/gh/chrisgarson/CTCraziesBlog@main/article-images/2026-07-10_093411.jpg"
-        tags={["Chicago", "Violent Crime", "Law Enforcement", "Democrat Party"]}
+        tags={["Chicago", "Violent Crime", "Law Enforcement"]}
       />
       <ArticleBlock
         headline="Just Another State Tax Hike by Democrats That Makes Already High California Gas Prices Even More Unaffordable"
         tinyUrl="https://tinyurl.com/kyd5bekt"
         xPostUrl="https://x.com/C3Heditor/status/2075588722507108512"
         imageSrc="https://cdn.jsdelivr.net/gh/chrisgarson/CTCraziesBlog@main/article-images/2026-07-10_093201.jpg"
-        tags={["California", "Gas Prices", "Economic Policy", "Democrat Party"]}
+        tags={["California", "Gas Prices", "Economic Policy"]}
       />
       <ArticleBlock
         headline="Company That Produced the Republican President Reagan Film Was De-Banked During the Biden Democrat Administration"
         tinyUrl="https://tinyurl.com/4hx2p2w6"
         xPostUrl="https://x.com/C3Heditor/status/2075587384280772631"
         imageSrc="https://cdn.jsdelivr.net/gh/chrisgarson/CTCraziesBlog@main/article-images/2026-07-10_092642.jpg"
-        tags={["Biden Administration", "Censorship", "Democrat Party"]}
+        tags={["Biden Administration", "Censorship"]}
       />
       <ArticleBlock
         headline="Just a Reminder to RINOs: Top Democrats Vow to Stack and Pack the Supreme Court, Plus Nuke the Senate Filibuster to Keep Power"
         tinyUrl="https://tinyurl.com/42nt5unn"
         xPostUrl="https://x.com/C3Heditor/status/2075586350309331159"
         imageSrc="https://cdn.jsdelivr.net/gh/chrisgarson/CTCraziesBlog@main/article-images/2026-07-10_092253.jpg"
-        tags={["Judiciary-Judicial", "Democrat Party", "U.S. Congress"]}
+        tags={["Judiciary-Judicial", "U.S. Congress"]}
       />
       <ArticleBlock
         headline="America's 'Entire Existence' Is a 'Crime,' Says Democrat Propagandist Nikole Hannah-Jones, the NY Times Creator of the Debunked '1619 Project'"
@@ -67,7 +67,7 @@ export default function Page2() {
         tinyUrl="https://tinyurl.com/4wtv5jtd"
         xPostUrl="https://x.com/C3Heditor/status/2075553988523553071"
         imageSrc="https://cdn.jsdelivr.net/gh/chrisgarson/CTCraziesBlog@main/article-images/2026-07-10_071402.jpg"
-        tags={["Israel", "Democrat Party", "Politics"]}
+        tags={["Israel", "Politics"]}
       />
       <ArticleBlock
         headline="Ex-Staffer for Massachusetts Democrat-Socialist Sen. Elizabeth Warren Attends Iran Ayatollah's Funeral, Calls Him 'Greatest Leader'"
@@ -88,35 +88,35 @@ export default function Page2() {
         tinyUrl="https://tinyurl.com/5bd8e8rw"
         xPostUrl="https://x.com/C3Heditor/status/2075547941264109573"
         imageSrc="https://cdn.jsdelivr.net/gh/chrisgarson/CTCraziesBlog@main/article-images/2026-07-10_065000.jpg"
-        tags={["Socialism", "Democrat Party", "Politics"]}
+        tags={["Socialism", "Politics"]}
       />
       <ArticleBlock
         headline="Democrats' and SPLC's Favorite (Sponsored?) &quot;White Supremacy&quot; Boogeymen Make Appearance at U.S. Capital's America 250 Celebration"
         tinyUrl="https://tinyurl.com/4junvkfe"
         xPostUrl="https://x.com/C3Heditor/status/2075545986546487414"
         imageSrc="https://cdn.jsdelivr.net/gh/chrisgarson/CTCraziesBlog@main/article-images/2026-07-10_064217.jpg"
-        tags={["SPLC", "Racial Issues", "Democrat Party"]}
+        tags={["SPLC", "Racial Issues"]}
       />
       <ArticleBlock
         headline="Most Americans Celebrated the Nation's 250th, but Democrats Who Hate America Used the Day to Bash the U.S."
         tinyUrl="https://tinyurl.com/54m6bens"
         xPostUrl="https://x.com/C3Heditor/status/2075543829638173111"
         imageSrc="https://cdn.jsdelivr.net/gh/chrisgarson/CTCraziesBlog@main/article-images/2026-07-10_063337.jpg"
-        tags={["Patriotism", "Democrat Party", "Culture War"]}
+        tags={["Patriotism", "Culture War"]}
       />
       <ArticleBlock
         headline="Poll Shows That Democrats Rate the 'BLM' Flag Above U.S. Flag"
         tinyUrl="https://tinyurl.com/m2thba44"
         xPostUrl="https://x.com/C3Heditor/status/2075543055252279499"
         imageSrc="https://cdn.jsdelivr.net/gh/chrisgarson/CTCraziesBlog@main/article-images/2026-07-10_063046.jpg"
-        tags={["Racial Issues", "Polling", "Patriotism", "Democrat Party"]}
+        tags={["Racial Issues", "Polling", "Patriotism"]}
       />
       <ArticleBlock
         headline="U.S. Top 10 List of Least-Educated Cities Includes Six From Democrat-Run California and Its Education System"
         tinyUrl="https://tinyurl.com/4km6wduh"
         xPostUrl="https://x.com/C3Heditor/status/2075556280698040605"
         imageSrc="https://cdn.jsdelivr.net/gh/chrisgarson/CTCraziesBlog@main/article-images/2026-07-10_062733.jpg"
-        tags={["California", "Education Policy", "Democrat Party"]}
+        tags={["California", "Education Policy"]}
       />
       <ArticleBlock
         headline="Minnesota Democrat Gov. Tim Walz Pardons Illegal Alien Who Raped a Child Repeatedly Thus Shielding the Immigrant From Deportation"
@@ -130,21 +130,21 @@ export default function Page2() {
         tinyUrl="https://tinyurl.com/mwyu6kaa"
         xPostUrl="https://x.com/C3Heditor/status/2075540195420565533"
         imageSrc="https://cdn.jsdelivr.net/gh/chrisgarson/CTCraziesBlog@main/article-images/2026-07-10_061913.jpg"
-        tags={["Illinois", "Sexual Misconduct", "Democrat Party"]}
+        tags={["Illinois", "Sexual Misconduct"]}
       />
       <ArticleBlock
         headline="What The Hell? The Platner and Swalwell Sexual Abuses Expose The Extreme Incompetence of Democrats' Candidate Vetting Process"
         tinyUrl="https://tinyurl.com/yc4vmrwe"
         xPostUrl="https://x.com/C3Heditor/status/2075538901435859264"
         imageSrc="https://cdn.jsdelivr.net/gh/chrisgarson/CTCraziesBlog@main/article-images/2026-07-10_061410.jpg"
-        tags={["Graham Platner", "Eric Swalwell", "Sexual Misconduct", "Democrat Party"]}
+        tags={["Graham Platner", "Eric Swalwell", "Sexual Misconduct"]}
       />
       <ArticleBlock
         headline="Why of Course! Democrat Nazi-Socialist Sexual Scandals Are The Fault of Trump, Per NY Times Political Experts"
         tinyUrl="https://tinyurl.com/2s3hnzef"
         xPostUrl="https://x.com/C3Heditor/status/2075537688703148266"
         imageSrc="https://cdn.jsdelivr.net/gh/chrisgarson/CTCraziesBlog@main/article-images/2026-07-10_060924.jpg"
-        tags={["Media Bias", "Sexual Misconduct", "Democrat Party"]}
+        tags={["Media Bias", "Sexual Misconduct"]}
       />
       </div>
       <Pagination currentPage={3} totalPages={61} />
