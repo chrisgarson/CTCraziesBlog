7fd32f303b0a3119dfd66745289f1e610e627cf6
chrisgarson <chrisgarson@zohomail.com>
2026-07-28 13:04:17 -0500

    Jul 28 batch: App.tsx (added Page66 route)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -64,6 +64,7 @@ import Page62 from "./pages/Page62";
 import Page63 from "./pages/Page63";
 import Page64 from "./pages/Page64";
 import Page65 from "./pages/Page65";
+import Page66 from "./pages/Page66";
 import Search from "./pages/Search";
 import TagResults from "./pages/TagResults";
 import TagsIndex from "./pages/TagsIndex";
@@ -136,6 +137,7 @@ function App() {
         <Route path="/page63" component={Page63} />
         <Route path="/page64" component={Page64} />
         <Route path="/page65" component={Page65} />
+        <Route path="/page66" component={Page66} />
         <Route path="/search" component={Search} />
         <Route path="/tags" component={TagsIndex} />
         <Route path="/tag/:tag" component={TagResults} />
